f2c69a186d3cc129e555ceee1e6bc03125aa02a4
Mohamed Hisham <hisham@mhisham.net>
2018-09-23 19:40:21 +0300

    GS

--- SignMainController.swift
+++ SignMainController.swift
@@ -14,9 +14,33 @@ class SignMainController: UIViewController {
             return //Exit
         }
         if(json["key"].stringValue == "success"){ // Other requests Success..
-            print("SUCCESS: \(json)")//Print the requiest in the console
-            let loginAsView = viewController.storyboard?.instantiateViewController(withIdentifier: "loginAs") as! LoginAsViewController // Login As View
+            print("SUCCESS: \(json)")
             
+            //If Any response has Full Name or Email save it
+            if json["results"]["user"]["full_name"].string != nil { //If Any response has Full Name save it in user prefrences
+                UserData.setFullName(json["results"]["user"]["full_name"].stringValue)
+            }
+            if json["results"]["user"]["email"].string != nil { //If Any response has Email save it in user prefrences
+                UserData.setEmail(json["results"]["user"]["email"].stringValue)
+            }
+            
+            //New Account Case
+            if(json["results"]["account_info"]["username"].string != nil){ // if response has username in account_info
+                let gsProfile = GsProfile()
+                gsProfile.accountNumber = json["results"]["account_info"]["username"].stringValue //Gs Number
+                gsProfile.accountPassword = json["results"]["account_info"]["password"].stringValue //GS Password
+                gsProfile.profileName = UserData.fullName() ?? "" //full Name from userDefaults
+                gsProfile.balance = json["results"]["account_info"]["balance"].float ?? 0
+                
+                let mainHomeView = viewController.storyboard?.instantiateViewController(withIdentifier: "mainView") as! MainTabBarController // MainView - Home
+                mainHomeView.userProfile = gsProfile // Pass Gs profile to Main
+                UserData.GsLoggedIn(gsProfile.accountNumber, gsPassword: gsProfile.accountPassword) // Save new Login number in User Defaults
+                UIApplication.shared.keyWindow?.rootViewController = mainHomeView
+                return
+            }
+            
+            //Login As Case
+            let loginAsView = viewController.storyboard?.instantiateViewController(withIdentifier: "loginAs") as! LoginAsViewController // Login As View
             if json["results"]["accounts"].arrayObject != nil { // IF there's accounts key in the results prepair it
                 let accounts = json["results"]["accounts"].arrayValue
                 var profileArr:[GsProfile] = []
@@ -39,13 +63,6 @@ class SignMainController: UIViewController {
                 loginAsView.gsNumbers = profileArr
             }
             
-            if json["results"]["user"]["full_name"].string != nil { //If Any response has Full Name save it in user prefrences
-                UserData.setFullName(json["results"]["user"]["full_name"].stringValue)
-            }
-            if json["results"]["user"]["email"].string != nil { //If Any response has Email save it in user prefrences
-                UserData.setEmail(json["results"]["user"]["email"].stringValue)
-            }
-            
             //New GULFSIP and New Kuwait Connect
             if json["results"]["ask_for_free_account"].bool != nil && json["results"]["ask_for_free_account"].bool == true{ // If Respnse has ask_for free_account and = true
                 loginAsView.newGsNumber = true // Pass true to the View
@@ -54,7 +71,7 @@ class SignMainController: UIViewController {
                 loginAsView.newKuwaitNumber = true // pass true to the view
             }
             viewController.navigationController?.pushViewController(loginAsView, animated: true) // Open Login As View
-            return //Exit
+            return
         }
         
     }
